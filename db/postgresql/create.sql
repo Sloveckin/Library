1098ccf8937c73289663ee4e67276139d88a4765
Sloveckin <Sloveckin2711@gmail.com>
2026-02-10 22:26:41 +0300

    rewrite create.sql

--- db/postgresql/create.sql
+++ db/postgresql/create.sql
@@ -1,12 +1,17 @@
 create table Books (
-    Id serial not null primary key ,
-    Name varchar(64) not null
+                       Id serial not null primary key ,
+                       Name varchar(64) not null
 );
 
 create table Authors (
-    Id serial not null primary key,
-    BookId integer,
-    Name varchar(64) not null,
+                         Id serial not null primary key,
+                         Name varchar(64) not null
+);
 
-    foreign key (BookId) references Books(Id)
+create table AuthorToBook(
+                             AuthorId integer,
+                             BookId integer,
+
+                             foreign key (AuthorId) references Authors(Id),
+                             foreign key (BookId) references Books(Id)
 );
