5eb39d703bff5d5c055a9a252459f34c57425c64
Sloveckin <Sloveckin2711@gmail.com>
2026-02-14 01:17:56 +0300

    write auth service mock tests

--- go.mod
+++ go.mod
@@ -2,9 +2,12 @@ module Library
 
 go 1.25
 
+require github.com/stretchr/testify v1.11.1
+
 require (
 	github.com/BurntSushi/toml v1.6.0 // indirect
 	github.com/ajg/form v1.6.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.13 // indirect
 	github.com/go-chi/chi/v5 v5.2.5 // indirect
 	github.com/go-chi/render v1.0.3 // indirect
@@ -19,6 +22,8 @@ require (
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
 	golang.org/x/crypto v0.47.0 // indirect
 	golang.org/x/sync v0.19.0 // indirect
 	golang.org/x/sys v0.40.0 // indirect
---
+++ internal/service/author/service_test.go
@@ -0,0 +1,249 @@
+package service_author
+
+import (
+	"Library/internal/model"
+	"Library/internal/service/author/mocks"
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/mock"
+)
+
+func TestAuthorServiceImpl_Create(t *testing.T) {
+	type fields struct {
+		authorRepository AuthorRepository
+	}
+	type args struct {
+		name string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *model.Author
+		wantErr bool
+	}{
+		{
+			name: "success",
+			args: args{
+				name: "Mark",
+			},
+			want:    &model.Author{Name: "Mark"},
+			wantErr: false,
+		},
+		{
+			name: "error",
+			args: args{
+				name: "Elon",
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			mockRepo := mocks.NewAuthorRepository(t)
+
+			if tt.name == "success" {
+
+				mockRepo.
+					On("ExistsByName", tt.args.name).
+					Return(false, nil)
+
+				mockRepo.
+					On("Create", mock.Anything).
+					Return(&model.Author{Name: tt.args.name}, nil)
+
+			} else {
+
+				mockRepo.
+					On("ExistsByName", tt.args.name).
+					Return(true, nil)
+
+			}
+
+			s := AuthorServiceImpl{
+				authorRepository: mockRepo,
+			}
+
+			got, err := s.Create(tt.args.name)
+
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Create() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Create() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestAuthorServiceImpl_Delete(t *testing.T) {
+	type args struct {
+		id string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "success",
+			args: args{
+				id: "1",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			mockRepo := mocks.NewAuthorRepository(t)
+
+			mockRepo.
+				On("ExistsById", tt.args.id).
+				Return(true, nil)
+
+			mockRepo.On("Delete", tt.args.id).Return(nil)
+
+			s := &AuthorServiceImpl{
+				authorRepository: mockRepo,
+			}
+
+			if err := s.Delete(tt.args.id); (err != nil) != tt.wantErr {
+				t.Errorf("Delete() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestAuthorServiceImpl_ExistsById(t *testing.T) {
+	type args struct {
+		id string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    bool
+		wantErr bool
+	}{
+		{
+			name: "success",
+			args: args{
+				id: "1",
+			},
+			want:    true,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			mockRepo := mocks.NewAuthorRepository(t)
+
+			mockRepo.
+				On("ExistsById", tt.args.id).
+				Return(true, nil)
+
+			s := &AuthorServiceImpl{
+				authorRepository: mockRepo,
+			}
+
+			got, err := s.ExistsById(tt.args.id)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ExistsById() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("ExistsById() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestAuthorServiceImpl_ExistsByName(t *testing.T) {
+	type args struct {
+		id string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    bool
+		wantErr bool
+	}{
+		{
+			name: "success",
+			args: args{
+				id: "Mark",
+			},
+			want:    true,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			mockRepo := mocks.NewAuthorRepository(t)
+
+			mockRepo.
+				On("ExistsByName", tt.args.id).
+				Return(true, nil)
+
+			s := &AuthorServiceImpl{
+				authorRepository: mockRepo,
+			}
+
+			got, err := s.ExistsByName(tt.args.id)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ExistsById() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("ExistsById() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestAuthorServiceImpl_Get(t *testing.T) {
+	type args struct {
+		id string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *model.Author
+		wantErr bool
+	}{
+		{
+			name: "success",
+			args: args{
+				id: "1",
+			},
+			want:    &model.Author{Id: "1", Name: "Mark"},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			mockRepo := mocks.NewAuthorRepository(t)
+			mockRepo.On("Get", tt.args.id).Return(tt.want, nil)
+
+			s := &AuthorServiceImpl{
+				authorRepository: mockRepo,
+			}
+
+			got, err := s.Get(tt.args.id)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Get() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Get() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
