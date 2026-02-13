ee9b0f4281b6d06c02de66b0be3203b57bba18b7
Sloveckin <Sloveckin2711@gmail.com>
2026-02-14 01:22:10 +0300

    delete extra whitespaces from create.sql

--- db/postgresql/create.sql
+++ db/postgresql/create.sql
@@ -1,17 +1,17 @@
 create table Books (
-                       Id serial not null primary key ,
-                       Name varchar(64) not null
+    Id serial not null primary key ,
+    Name varchar(64) not null
 );
 
 create table Authors (
-                         Id serial not null primary key,
-                         Name varchar(64) not null
+    Id serial not null primary key,
+    Name varchar(64) not null
 );
 
 create table AuthorToBook(
-                             AuthorId integer,
-                             BookId integer,
+    AuthorId integer,
+    BookId integer,
 
-                             foreign key (AuthorId) references Authors(Id),
-                             foreign key (BookId) references Books(Id)
+    foreign key (AuthorId) references Authors(Id),
+    foreign key (BookId) references Books(Id)
 );
