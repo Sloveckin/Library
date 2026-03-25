beee9627a66a5fabf0c74832b533051bac082df6
Gleb Pimenov <gleb.pimenov7@gmail.com>
2026-03-25 23:22:14 +0300

    fix tests

--- server/internal/service/author/mocks/AuthorRepository.go
+++ server/internal/service/author/mocks/AuthorRepository.go
@@ -147,6 +147,66 @@ func (_m *AuthorRepository) Get(id string) (*model.Author, error) {
 	return r0, r1
 }
 
+// GetByName provides a mock function with given fields: name
+func (_m *AuthorRepository) GetByName(name string) (*model.Author, error) {
+	ret := _m.Called(name)
+
+	if len(ret) == 0 {
+		panic("no return value specified for GetByName")
+	}
+
+	var r0 *model.Author
+	var r1 error
+	if rf, ok := ret.Get(0).(func(string) (*model.Author, error)); ok {
+		return rf(name)
+	}
+	if rf, ok := ret.Get(0).(func(string) *model.Author); ok {
+		r0 = rf(name)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*model.Author)
+		}
+	}
+
+	if rf, ok := ret.Get(1).(func(string) error); ok {
+		r1 = rf(name)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// Update provides a mock function with given fields: id, name
+func (_m *AuthorRepository) Update(id string, name string) (*model.Author, error) {
+	ret := _m.Called(id, name)
+
+	if len(ret) == 0 {
+		panic("no return value specified for Update")
+	}
+
+	var r0 *model.Author
+	var r1 error
+	if rf, ok := ret.Get(0).(func(string, string) (*model.Author, error)); ok {
+		return rf(id, name)
+	}
+	if rf, ok := ret.Get(0).(func(string, string) *model.Author); ok {
+		r0 = rf(id, name)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*model.Author)
+		}
+	}
+
+	if rf, ok := ret.Get(1).(func(string, string) error); ok {
+		r1 = rf(id, name)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // NewAuthorRepository creates a new instance of AuthorRepository. It also registers a testing interface on the mock and a cleanup function to assert the mocks expectations.
 // The first argument is typically a *testing.T value.
 func NewAuthorRepository(t interface {
